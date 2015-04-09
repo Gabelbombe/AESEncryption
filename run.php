6f1a324f311b5490b304f568985c25bdf69805bb
ehime <dodomeki@gmail.com>
2015-04-09 13:18:36 -0700

    Wrap up, fixed IO for CLI, test with php -f run.php

--- run.php
+++ run.php
@@ -11,23 +11,26 @@ try
     $data = $aes->encrypt($string, true);   // expecting return of a raw byte string
     $decr = $aes->decrypt($data);           // expecting the return of "hello world"
 
-    var_export($decr);
+    echo "Expecting 'hello world': {$decr}\n";
 
     // encrypt something else with a different key
     $aes->SetKey('my other secret key');    // exception can be thrown here if the class is not supported
     $data2 = $aes->encrypt($string);        // return the return of a lowercase hexit string
     $decr  = $aes->decrypt($data2);         // expecting the return of "hello world"
 
-    var_export($decr);
+    echo "Expecting 'hello world': {$decr}\n";
 
     // proof that the key was changed
     $decr = $aes->decrypt($data);           // expecting return of Boolean False
-    var_export($decr);
+
+    echo "Expecting 'false': "; var_export($decr);
+    echo "\n";
 
     // reset the key back
     $aes->SetKey($key);                     // exception can be thrown here if the class is not supported
     $decr = $aes->decrypt($data);           // expecting hello world
-    var_export($decr);
+
+    echo "Expecting 'hello world': {$decr}\n";
 }
 
 catch (Exception $e)
